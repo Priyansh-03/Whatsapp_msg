0759a2f423f1b33fb9fc873ba0745534fe124cec
Priyansh-03 <114228164+Priyansh-03@users.noreply.github.com>
2023-07-03 11:41:31 +0530

    Add files via upload

---
+++ send_whatsapp.py
@@ -0,0 +1,41 @@
+# import pywhatkit
+
+# # Set the contact's phone number and the message
+# phone_number = "+91132545"  # Replace with the recipient's phone number
+# message = " "
+# # Send the message to the contact on WhatsApp
+# pywhatkit.sendwhatmsg(phone_number, message, 0, 0)
+
+import pyautogui
+import pywhatkit
+import time
+import webbrowser as wb
+wb.open("web.whatsapp.com")
+time.sleep(10)
+# phone_number = "+91xxx"  # replace with your phone number
+message = ""
+
+for i in range(100):
+    pyautogui.press("H")
+    pyautogui.press("a")
+    pyautogui.press("p")
+    pyautogui.press("p")
+    pyautogui.press("p")
+    pyautogui.press("y")
+    pyautogui.press("space")
+    pyautogui.press("B")
+    pyautogui.press("i")
+    pyautogui.press("r")
+    pyautogui.press("t")
+    pyautogui.press("h")
+    pyautogui.press("d")
+    pyautogui.press("a")
+    pyautogui.press("y")
+    pyautogui.press("space")
+    pyautogui.press("N")
+    pyautogui.press("a")
+    pyautogui.press("m")
+    pyautogui.press("e")
+    pyautogui.press("enter") 
+    
+
